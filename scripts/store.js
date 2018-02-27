797ca1d2b0325b93ee9a1affb31af7df0423529b
ssheaks <sarah.sheaks@gmail.com>
2018-02-27 13:28:24 -0800

    create store.js

---
+++ scripts/store.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* global  */
+
+// Store
+// Exposes a videos array
+// Exposes a setVideos function that simply receives videos and sets it to this.videos
+
+const store = function() {
+  let videos = [];
+  const setVideos = function(videos) {
+    this.videos = videos;
+  };
+  return{
+    videos,
+    setVideos
+  };
+}();
+
